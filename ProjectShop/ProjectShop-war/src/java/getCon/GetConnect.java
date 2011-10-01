f159eed886e8d13143119bacf2a0c9c6d6a87114
Jura Bernatskiy <burnaccki90@mail.ru>
2011-10-02 02:52:36 +0400

    jkjk

---
+++ ProjectShop/ProjectShop-war/src/java/getCon/GetConnect.java
@@ -0,0 +1,40 @@
+package getCon;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yra
+ */
+import java.sql.*;
+import oracle.jdbc.pool.*;
+import javax.naming.*;
+import java.util.*;
+
+import javax.sql.DataSource;
+import com.sun.jndi.cosnaming.*;
+public class GetConnect  {
+    static private String user= "JURA";
+    static private String password = "12zxfg54";
+    static private String drivType = "thin";
+    static private String serverName = "localhost";
+    static private String serviceName = "XE";
+    static private int port = 1521;
+    /*public static void main(String args[])throws SQLException, NamingException {
+        Properties prop = new Properties( );
+     //   prop.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.cosnaming.CNCtxFactory");
+    //    prop.put(Context.PROVIDER_URL, "http://localhost:34989");
+        DataSource ds = (DataSource) new InitialContext().lookup("jdbc/InternetShop");
+        
+        Connection conn = ds.getConnection( );
+//...
+        conn.close( );
+
+    }*/
+    public static DataSource getDataSource() throws SQLException, NamingException{
+      DataSource ds = (DataSource) new InitialContext().lookup("jdbc/InternetShop");
+      return ds;
+    }
+}
