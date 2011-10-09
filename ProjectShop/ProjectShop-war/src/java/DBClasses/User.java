790fc38a36c44496fe952c0ca7cae898617aafa0
Jura Bernatskiy <burnaccki90@mail.ru>
2011-10-09 17:24:09 +0400

    тут пример того как сделать один сервлет

---
+++ ProjectShop/ProjectShop-war/src/java/DBClasses/User.java
@@ -0,0 +1,116 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DBClasses;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author Yra
+ */
+public class User {
+private int id_user;
+private String name;
+private String surname;
+private String otchestvo;
+private String nik;
+private String password;
+private Date born;
+private String phone;
+private String email;
+private int id_role;
+public User(ResultSet rs) throws SQLException{
+    //SimpleDateFormat formt = new SimpleDateFormat("dd MM yyyy");
+      // Date born = formt.parse(brn);
+    setId_user(rs.getInt(1));
+    setName(rs.getString(2));
+    setSurname(rs.getString(3));
+    setOtchestvo(rs.getString(4));
+    setNik(rs.getString(5));
+    setPassword(rs.getString(6));
+    setBorn(rs.getDate(7));
+    setPhone(rs.getString(8));
+    setEmail(rs.getString(9));
+    setId_role(rs.getInt(10));
+}
+public User(String name, String surname, String otchestvo,String nik,String password, String phone, String email, int id_role) throws SQLException{
+    //SimpleDateFormat formt = new SimpleDateFormat("dd MM yyyy");
+      // Date born = formt.parse(brn);
+  /*  setName(name);
+    setSurname(surname);
+    setOtchestvo(otchestvo);
+    setNik(nik);
+    setPassword();
+    setBorn(rs.getDate(7));
+    setPhone(rs.getString(8));
+    setEmail(rs.getString(9));
+    setId_role(rs.getInt(10));*/
+}
+public int getId_user(){
+   return id_user;
+}
+public void setId_user(int nid_user){
+    id_user=nid_user;
+}
+public String getName(){
+    return name;
+    }
+public void setName(String nname){
+    name=nname;
+}
+public String getSurname(){
+    return surname;
+    }
+public void setSurname(String nsurname){
+    surname=nsurname;
+}
+public String getOtchestvo(){
+    return surname;
+    }
+public void setOtchestvo(String notchestvo){
+    otchestvo=notchestvo;
+}
+public String getNik(){
+    return nik;
+    }
+public void setNik(String nnik){
+    nik=nnik;
+}
+public String getPassword(){
+    return password;
+    }
+public void setPassword(String npassword){
+    password=npassword;
+}
+public Date getBorn(){
+    return born;
+    }
+public void setBorn(Date nborn){
+    born=nborn;
+}
+public String getPhone(){
+    return phone;
+    }
+public void setPhone(String nphone){
+    phone=nphone;
+}
+public String getEmail(){
+    return email;
+    }
+public void setEmail(String nemail){
+    email=nemail;
+}
+public int getId_role(){
+    return id_role;
+    }
+public void setId_role(int nid_role){
+    id_role=nid_role;
+}
+public String toString(){
+    return surname+" "+name+" "+otchestvo;
+}
+}
--- ProjectShop/ProjectShop-war/src/java/DBManager/DBManager.java
+++ ProjectShop/ProjectShop-war/src/java/DBManager/DBManager.java
@@ -14,36 +14,68 @@ import javax.naming.*;
 import java.util.*;
 import java.util.Date;
 import javax.sql.DataSource;
-import com.sun.jndi.cosnaming.*;
 import java.sql.PreparedStatement;
 import exceptions.NikNameException;
-public class DBManager  {
- 
-     public static Connection getConnection() throws SQLException, NamingException{
-      DataSource ds = (DataSource) new InitialContext().lookup("jdbc/InternetShop");
-      return ds.getConnection();}
+import DBClasses.User;
 
-    public static void addUser(String name, String surname,String otchestvo,String nik, String password, Date born, String phone, String email, int id_role) throws SQLException, NamingException, NikNameException {
-      Connection conn= getConnection();
-      PreparedStatement pst = conn.prepareStatement("SELECT * FROM \"USER\" WHERE NIK = ?");
-      pst.setString(1, nik);
-      ResultSet rs = pst.executeQuery();
-      boolean b = rs.next();
-      if(b){
-          throw new NikNameException();}
-      pst = conn.prepareStatement("INSERT INTO \"USER\" "+"(NAME,SURNAME,OTCHESTVO,NIK,PASSWORD,BORN,PHONE,EMAIL,ID_ROLE)"+"VALUES(?,?,?,?,?,?,?,?,?)");
-      pst.setString(1, name);
-      pst.setString(2, surname);
-      pst.setString(3, otchestvo);
-      pst.setString(4, nik);
-      pst.setString(5, password);
-      pst.setDate(6,new java.sql.Date(born.getTime()));
-      pst.setString(7, phone);
-      pst.setString(8, email);
-      pst.setInt(9, id_role);
-      pst.execute();
-      conn.commit();
-      conn.close();
-      
-  }
+public class DBManager {
+
+    public static Connection getConnection() throws SQLException, NamingException {
+        DataSource ds = (DataSource) new InitialContext().lookup("jdbc/InternetShop");
+        return ds.getConnection();
+    }
+
+    public static User findUserByNik(String nik) throws SQLException, NamingException {
+        Connection conn = getConnection();
+        User usr = null;
+        try {
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM \"USER\" WHERE NIK = ?");
+            pst.setString(1, nik);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                usr = new User(rs);
+            }
+        } finally {
+            conn.close();
+        }
+        return usr;
+    }
+    public static void updateUserbyNik(User usr) throws SQLException, NamingException{
+        Connection conn = getConnection();
+        PreparedStatement pst = conn.prepareStatement("UPDATE \"USER\" SET NAME =?, SURNAME =?, OTCHESTVO =?, NIK =?, PASSWORD =?, BORN=?,PHONE=?,EMAIL=?,ID_ROLE");
+        pst.setString(1, usr.getName());
+        pst.setString(2, usr.getSurname());
+        pst.setString(3, usr.getOtchestvo());
+        pst.setString(4, usr.getNik());
+        pst.setString(5, usr.getPassword());
+        pst.setDate(6, new java.sql.Date(usr.getBorn().getTime()));
+        pst.setString(7, usr.getPhone());
+        pst.setString(8, usr.getEmail());
+        pst.setInt(9,usr.getId_role());
+        pst.execute();
+        conn.commit();
+    }
+    public static void addUser(String name, String surname, String otchestvo, String nik, String password, Date born, String phone, String email, int id_role) throws SQLException, NamingException, NikNameException {
+        Connection conn = getConnection();
+        PreparedStatement pst = conn.prepareStatement("SELECT * FROM \"USER\" WHERE NIK = ?");
+        pst.setString(1, nik);
+        ResultSet rs = pst.executeQuery();
+        //rs.next();
+        if (rs.next()) {
+            throw new NikNameException();
+        }
+        pst = conn.prepareStatement("INSERT INTO \"USER\" " + "(NAME,SURNAME,OTCHESTVO,NIK,PASSWORD,BORN,PHONE,EMAIL,ID_ROLE)" + "VALUES(?,?,?,?,?,?,?,?,?)");
+        pst.setString(1, name);
+        pst.setString(2, surname);
+        pst.setString(3, otchestvo);
+        pst.setString(4, nik);
+        pst.setString(5, password);
+        pst.setDate(6, new java.sql.Date(born.getTime()));
+        pst.setString(7, phone);
+        pst.setString(8, email);
+        pst.setInt(9, id_role);
+        pst.execute();
+        conn.commit();
+        conn.close();
+    }
 }
